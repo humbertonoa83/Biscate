8d44cd2d90dd870fd6e9ba45404c79c52bb6f440
humberto83 <humbertonoa83@gmail.com>
2020-12-29 16:41:42 +0200

    Revert "Pagina inicial"
    This reverts commit b6c19ab38db3804e9187e5fa98d3852987b883a8.

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-
-Route::get('/','App\Http\Controllers\LandingPageController@index')->name('landing.page');
+Route::get('/', function () {
+    return view('welcome');
+});
